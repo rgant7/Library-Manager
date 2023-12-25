842377f0c9ed66574d73343ba065f1ada8a59262
Rohan Ganta <rohanganta7@gmail.com>
2023-12-25 15:41:13 -0600

    implemented getters and setters for member.java

--- Member.java
+++ Member.java
@@ -2,24 +2,66 @@ import java.util.HashMap;
 
 public class Member {
     private String name;
+    private String memberId;
+    private int bookCount;
     private HashMap<String, BookCopy> borrowedBooks;
 
-    public Member(String name) {
+    public Member(String name, String memberId) {
         this.name = name;
+        this.memberId = memberId;
+        this.bookCount = 0;
         this.borrowedBooks = new HashMap<>();
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public String getMemberId() {
+        return this.memberId;
+    }
+
+    public int getBookCount() {
+        return this.bookCount;
+    }
+
+    public HashMap<String, BookCopy> getBorrowedBooks() {
+        return this.borrowedBooks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setBookCount(int bookCount) {
+        this.bookCount = bookCount;
+    }
+
+    public void setBorrowedBooks(HashMap<String, BookCopy> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
     public synchronized void checkoutBook(Library library, String ISBN, String copyId){
+        if(bookCount >= 5){
+            System.out.println("Error: Members can only have 5 checked out books at a time. Please return a books");
+            return;
+        }
         library.checkoutCopy(ISBN, copyId);
 
         Book book = library.getBooks().get(ISBN);
         BookCopy copy = book.getCopies().get(copyId);
         
         borrowedBooks.put(copyId, copy);
+        bookCount++;
     }
 
     public synchronized void returnBook(Library library, String ISBN, String copyId){
         library.returnCopy(ISBN, copyId);
         borrowedBooks.remove(copyId);
+        bookCount--;
     }
 }
